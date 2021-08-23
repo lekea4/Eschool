96bdb42a84722b935288ea69c9fbe9958606bb03
Adeleke Ayinde <lekea4@gmail.com>
2021-08-23 17:59:11 +0100

    Created the CourseTeacher Model/Entity

---
+++ Eschool/Models/CourseTeacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eschool.Models
+{
+    public class CourseTeacher
+    {
+        public int CourseId { get; set; }
+
+        public long TeacherId { get; set; }
+
+        public Course AssignedCourse { get; set; }
+
+        public Teacher AssignedTeacher { get; set; }
+    }
+}

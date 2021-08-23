c1606bceb852f68e1228a5e8ed8fa83ce40eb328
Adeleke Ayinde <lekea4@gmail.com>
2021-08-23 18:00:03 +0100

    Create the DBContext class of EschoolContext Model/Entity

---
+++ Eschool/Models/EschoolContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eschool.Models
+{
+    public class EschoolContext : DbContext
+    {
+
+        public EschoolContext(DbContextOptions<EschoolContext> options) : base (options)
+        {   
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CourseTeacher>()
+                .HasKey(ct => new { ct.TeacherId, ct.CourseId});
+        }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<CourseTeacher> CourseTeachers { get; set; }
+    }
+}

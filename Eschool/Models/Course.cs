1284d9be11417819b7f51efe6ab13eb85d8014fe
Adeleke Ayinde <lekea4@gmail.com>
2021-08-23 17:56:22 +0100

    Created the Courses Model/Entity

---
+++ Eschool/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eschool.Models
+{
+    public class Course
+    {
+        public long Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Title { get; set; }
+
+        public int Units { get; set; }
+
+    }
+}
